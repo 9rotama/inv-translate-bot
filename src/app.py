501b4160d1265c9dd539c3195f15bf68d1f9f997
9rotama <aoim99.sc@gmail.com>
2022-06-04 16:03:17 +0900

    翻訳APIをGASに移行, 原文も出力するようにした

--- src/app.py
+++ src/app.py
@@ -11,7 +11,7 @@ class BotClient(discord.Client):
 
     async def on_ready(self):
         print(f'We have logged in as {self.user}')
-        await self.change_presence(activity=discord.Game(name="^helpでヘルプ表示"))
+        await self.change_presence(activity=discord.Game(name=command_prefix+"helpでヘルプ表示"))
 
     async def on_message(self, message):
         channel_id = message.channel.id
@@ -49,6 +49,12 @@ class BotClient(discord.Client):
                     embed.set_author(name=self.user.name, icon_url=self.user.avatar_url)
                     await message.channel.send(embed=embed)
                     return
+                if len(args) > 10:
+                    embed = discord.Embed(title='set', description='中継言語に設定できるのは１０ヶ国語までです')
+                    embed.set_author(name=self.user.name, icon_url=self.user.avatar_url)
+                    await message.channel.send(embed=embed)
+                    self.channels[channel_id] = ["en"]
+                    return
                 else:
                     for arg in args:
                         if not arg in trans.LANGUAGES:
@@ -66,7 +72,7 @@ class BotClient(discord.Client):
                 return
 
         if message.content.startswith(command_prefix+'help'):
-            desc = '使い方:\n```'+command_prefix+'start```翻訳開始\n```'+command_prefix+'set```現在設定されている中継言語を表示\n```'+command_prefix+'set [1番目の言語コード [2番めの言語コード] ...```中継する言語を設定\n言語コードの表→https://cloud.google.com/translate/docs/languages?hl=ja\n```'+command_prefix+'stop```翻訳を終了'
+            desc = '使い方:\n```'+command_prefix+'start```翻訳開始\n```'+command_prefix+'set```現在設定されている中継言語を表示\n```'+command_prefix+'set [1番目の言語コード [2番めの言語コード] ...```中継する言語を設定\n※10ヶ国語まで設定できます\n言語コードの表→https://cloud.google.com/translate/docs/languages?hl=ja\n```'+command_prefix+'stop```翻訳を終了'
 
             embed = discord.Embed(title='help', description=desc)
             embed.set_author(name=self.user.name, icon_url=self.user.avatar_url)
@@ -82,10 +88,10 @@ class BotClient(discord.Client):
             else:
                 name = message.author.name
 
-            result = trans.trans_loop(tmp, self.channels[channel_id])
-            embed = discord.Embed(description=result)
+            result = trans.trans(tmp, self.channels[channel_id])
+            embed = discord.Embed(description=result+"\n\n||原文:" + tmp + "||")
             embed.set_author(name=name, icon_url=message.author.avatar_url)
-            embed.set_footer(text="translated by " + self.user.name, icon_url=self.user.avatar_url)
+            embed.set_footer(text="ja -> " + ' -> '.join(self.channels[channel_id]) + " -> ja", icon_url=self.user.avatar_url)
             await message.channel.send(embed=embed)
             return
 
